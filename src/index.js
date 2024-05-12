8699cf194aff81baa39b503af715a8f53c3d9dae
An Thanh Le <57203593+anlethanh048@users.noreply.github.com>
2024-05-12 11:50:17 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
+
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';  
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'; 
+import WebFont from 'webfontloader';
 
+WebFont.load({
+  google: {
+    families: ['Tangerine:300,400,500,700', 'Open Sans'] // Khai báo font từ Google Fonts
+  },
+  active: function() {
+    // Hành động khi tất cả các font đã được nạp thành công
+  },
+  inactive: function() {
+    // Hành động nếu không có font nào được nạp thành công
+  }
+}); 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
